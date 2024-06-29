40201c7c341f186dea7106f54c7d20cc555dca79
Tameem2004 <mdtameem.gazi2004@gmail.com>
2024-06-29 15:46:11 +0530

    token created

---
+++ TokenCreate/mint.ts
@@ -0,0 +1,38 @@
+import { percentAmount, generateSigner, signerIdentity, createSignerFromKeypair } from '@metaplex-foundation/umi'
+import { TokenStandard, createAndMint, mplTokenMetadata } from '@metaplex-foundation/mpl-token-metadata'
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+import secret from './guideSecret.json';
+
+const umi = createUmi('https://api.devnet.solana.com'); //Replace with your QuickNode RPC Endpoint
+
+const userWallet = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(secret));
+const userWalletSigner = createSignerFromKeypair(umi, userWallet);
+
+const metadata = {
+    name: "CodeBounty",
+    symbol: "CBT",
+    uri: "https://github.com/Tameem2004/Tameem2004/blob/main/token.json",
+};
+
+const mint = generateSigner(umi);
+umi.use(signerIdentity(userWalletSigner));
+umi.use(mplTokenMetadata())
+
+createAndMint(umi, {
+    mint,
+    authority: umi.identity,
+    name: metadata.name,
+    symbol: metadata.symbol,
+    uri: metadata.uri,
+    sellerFeeBasisPoints: percentAmount(0),
+    decimals: 8,
+    amount: 10000000000000_000,
+    tokenOwner: userWallet.publicKey,
+    tokenStandard: TokenStandard.Fungible,
+}).sendAndConfirm(umi)
+    .then(() => {
+        console.log("Successfully minted 100 million tokens (", mint.publicKey, ")");
+    })
+    .catch((err) => {
+        console.error("Error minting tokens:", err);
+    });
